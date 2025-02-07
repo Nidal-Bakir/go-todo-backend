83d0ae9d08ea4fa4893d495926645b13854bf6a8
Nidal Bakir <nidal.bakir.edu@gmail.com>
2025-02-07 19:59:05 +0300

    update the RedisSlidingWindowLimiter code to use Hash rather than SortedSet & update pkgs

--- internal/middleware/ratelimiter/redis_ratelimiter/redis_sliding_window.go
+++ internal/middleware/ratelimiter/redis_ratelimiter/redis_sliding_window.go
@@ -2,7 +2,8 @@ package redis_ratelimiter
 
 import (
 	"context"
-	"fmt"
+	"slices"
+	"strconv"
 	"time"
 
 	"github.com/Nidal-Bakir/go-todo-backend/internal/middleware/ratelimiter"
@@ -23,64 +24,62 @@ func _slidingWindowAllow(ctx context.Context, key string, l *redisRatelimiter) (
 	perWindow := l.conf.RequestsPerTimeFrame
 	window := l.conf.TimeFrame
 	rdb := l.rdb
-	log := zerolog.Ctx(ctx).With().Str("key", key).Int("per_window", perWindow).Dur("window", window).Logger()
+	zerolog := zerolog.Ctx(ctx).With().Str("key", key).Int("per_window", perWindow).Dur("window", window).Logger()
 
-	currentTimeMicro := time.Now().UnixMicro()
-	windowStartTimeMicro := currentTimeMicro - window.Microseconds()
-
-	pip := rdb.TxPipeline()
-
-	pip.ZRemRangeByScore(ctx, key, "0", fmt.Sprint(windowStartTimeMicro))
-	pip.ZCard(ctx, key)
-
-	pipRes, err := pip.Exec(ctx)
+	rate, err := rdb.HLen(ctx, key).Result()
 	if err != nil {
-		log.Err(err).Msg("Can't rate limit, got an error from redis while exec the TxPipeline. Rejecting the request")
-		return false, window
-	}
-
-	redisIntCmd, ok := pipRes[len(pipRes)-1].(*redis.IntCmd)
-	if !ok {
-		log.Err(err).Msg("Can't cast the Pip result Cmder to IntCmd, cast error. Rejecting the request")
-		return false, window
-	}
-
-	rate, err := redisIntCmd.Result()
-	if err != nil {
-		log.Err(err).Msg("Can't rate limit, unable to get the rate from the ZCard from TxPipeline result. Rejecting the request")
+		zerolog.Err(err).Msg("Can't rate limit, unable to get the rate using the HLen on a hash")
 		return false, window
 	}
 
 	if rate >= int64(perWindow) {
-		resultSetData, err := rdb.ZRangeArgsWithScores(ctx, redis.ZRangeArgs{
-			Key:     key,
-			Start:   "-inf",
-			Stop:    "+inf",
-			ByScore: true,
-			Offset:  0,
-			Count:   1,
-		}).Result()
-
-		if err != nil || len(resultSetData) == 0 {
-			if err != nil {
-				log.Err(err).Msg("Can't calc the remaining time, sending window")
-			}
-			return false, window
-		}
-
-		remainingTime := int64(resultSetData[0].Score) - windowStartTimeMicro
-		remainingTimeMicro := time.Microsecond * time.Duration(remainingTime)
-		return false, remainingTimeMicro
+		return false, _calcRemainingTimeForSlidingWindow(ctx, key, window, rdb, zerolog)
 	}
 
-	pip = rdb.TxPipeline()
-	pip.ZAdd(ctx, key, redis.Z{Score: float64(currentTimeMicro), Member: fmt.Sprint(currentTimeMicro)})
-	pip.Expire(ctx, key, window)
-	pipRes, err = pip.Exec(ctx)
+	fieldKey := strconv.FormatInt(time.Now().UnixMilli(), 10)
+	didAdd, err := rdb.HSetNX(ctx, key, fieldKey, "").Result()
+	if err != nil || !didAdd {
+		zerolog.Err(err).Msg("Can't rate limit, unable to register new requst in redis hash table")
+		return false, window
+	}
+
+	err = rdb.HExpire(ctx, key, window, fieldKey).Err()
 	if err != nil {
-		log.Err(err).Msg("Can't rate limit, got an error from redis while exec the TxPipeline. Rejecting the request")
+		zerolog.Err(err).Msg("Can't rate limit, unable to set expire for the hash table field")
 		return false, window
 	}
 
 	return true, 0
+}
+
+func _calcRemainingTimeForSlidingWindow(ctx context.Context, key string, window time.Duration, rdb *redis.Client, zerolog zerolog.Logger) time.Duration {
+
+	fields, err := rdb.HKeys(ctx, key).Result()
+	if err != nil {
+		zerolog.Err(err).Msg("Can't get all fields in order to calc the remaining time, sending window")
+		return window
+	}
+
+	// if there is not fields that mean the user can request at this moment,
+	// and all the Hash fields has been removed due to the fields TTL
+	if len(fields) == 0 {
+		return 0
+	}
+
+	slices.Sort(fields)
+
+	res, err := rdb.HPTTL(ctx, key, fields[0]).Result()
+	if err != nil {
+		zerolog.Err(err).Msg("Can't get the TTL for a hash feild, sending window")
+		return window
+	}
+
+	// if there is no result then the key must have been expired and removed
+	// so the user can call the API at this moment
+	if len(res) == 0 {
+		return 0
+	}
+
+	remainingTime := time.Millisecond * time.Duration(res[0])
+	return remainingTime
 }
