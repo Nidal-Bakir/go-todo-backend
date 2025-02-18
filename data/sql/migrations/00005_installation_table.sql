9c0648773d18ff2a04ca048bf0d8cf3b8d8629c7
Nidal Bakir <nidal.bakir.edu@gmail.com>
2025-02-18 22:12:19 +0300

    wip

--- data/sql/migrations/00005_installation_table.sql
+++ data/sql/migrations/00005_installation_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE installation (
     id SERIAL PRIMARY KEY NOT NULL,
     installation_id UUID UNIQUE NOT NULL,
     notification_token VARCHAR(200),
-    locale VARCHAR(16) NOT NULL CHECK (length(locale) >= 2),
+    locale VARCHAR(16) NOT NULL CHECK (length (locale) >= 2),
     timezone_offset_in_minutes INTEGER NOT NULL,
     device_manufacturer VARCHAR(50) NOT NULL,
     device_os VARCHAR(50) NOT NULL,
@@ -12,12 +12,12 @@ CREATE TABLE installation (
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     deleted_at TIMESTAMPTZ,
-    attach_to INTEGER NULL REFERENCES users(id),
-    last_attach_to INTEGER NULL REFERENCES users(id)
+    attach_to INTEGER REFERENCES users (id),
+    last_attach_to INTEGER REFERENCES users (id)
 );
 
 CREATE TRIGGER update_installation_updated_at_column BEFORE
-UPDATE ON installation FOR EACH ROW EXECUTE PROCEDURE trigger_set_updated_at_column();
+UPDATE ON installation FOR EACH ROW EXECUTE PROCEDURE trigger_set_updated_at_column ();
 
 -- +goose Down
 DROP TABLE installation;
--- data/sql/migrations/00007_otp_table.sql
+++ data/sql/migrations/00007_otp_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE otp (
     id SERIAL PRIMARY KEY NOT NULL,
-    code VARCHAR(10) NOT NULL CHECK (length(code) >= 2),
+    code VARCHAR(10) NOT NULL CHECK (length (code) >= 2),
     hit_count SMALLINT NOT NULL CHECK (hit_count >= 0),
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
@@ -9,13 +9,13 @@ CREATE TABLE otp (
     deleted_at TIMESTAMPTZ,
     -- the intent is for checking why we sent the otp code (creating account, forget password, etc..)
     -- you need to check it to make sure that tha operation that you are about to do is what the code was sent for.
-    intent VARCHAR(50) NOT NULL CHECK (length(intent) >= 1),
-    otp_for INTEGER NOT NULL REFERENCES login_option(id),
-    using_session_id INTEGER NOT NULL REFERENCES session(id)
+    intent VARCHAR(50) NOT NULL CHECK (length (intent) >= 1),
+    otp_for INTEGER REFERENCES login_option (id),
+    using_session_id INTEGER REFERENCES session (id)
 );
 
 CREATE TRIGGER update_otp_updated_at_column BEFORE
-UPDATE ON otp FOR EACH ROW EXECUTE PROCEDURE trigger_set_updated_at_column();
+UPDATE ON otp FOR EACH ROW EXECUTE PROCEDURE trigger_set_updated_at_column ();
 
 -- +goose Down
 DROP TABLE otp;
